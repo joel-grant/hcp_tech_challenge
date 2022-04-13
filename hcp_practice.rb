32ea335910efad2503f0530430df8be9cab0b7e2
Joel Grant <joelmgrant@me.com>
2022-04-12 20:46:23 -0600

    Refactor to cover sad path of an empty string

--- hcp_practice.rb
+++ hcp_practice.rb
@@ -7,17 +7,20 @@
 # Ignors Symbols
   # Could manipulate the string before iterating OR user a conditional.
 
-def count_letters(word)
+def count_letters(string)
   # Create the hash with "0" passed to .new to avoid some further logic while incrementing.
   letter_counts = Hash.new(0)
-  # En lieu of a conditional block, I decided to change the string to only allow include the lowercase characters.
-  word_with_only_letters = word.downcase.gsub!(/[^a-z]/, "")
-  # Now that the string only has lowercase letters, I wanted to use an array for easy iterating.
-  word_array = word_with_only_letters.split("")
-  # Check each letter
-  word_array.each do |letter|
-    # Letter/key value gets incremented whether that key previously existed or not...no need to check first.
-    letter_counts[letter] += 1
+  # Prevents a sad path/edge case if the string happens to be empty.
+  if !string.empty?
+    # En lieu of a conditional block, I decided to change the string to only allow include the lowercase characters.
+    word_with_only_letters = string.downcase.gsub(/[^a-z]/, "")
+    # Now that the string only has lowercase letters, I wanted to use an array for easy iterating.
+    word_array = word_with_only_letters.split("")
+    # Check each letter
+    word_array.each do |letter|
+      # Letter/key value gets incremented whether that key previously existed or not...no need to check first.
+      letter_counts[letter] += 1
+    end
   end
   # Explicit return value due to using each as my enumerable.
   letter_counts
--- hcp_practice_without_comments.rb
+++ hcp_practice_without_comments.rb
@@ -1,12 +1,16 @@
-def count_letters(word)
+def count_letters(string)
   letter_counts = Hash.new(0)
-  word_with_only_letters = word.downcase.gsub!(/[^a-z]/, "")
-  word_array = word_with_only_letters.split("")
-  word_array.each do |letter|
-    letter_counts[letter] += 1
+  # Added conditional in case the string is empty
+  if !string.empty?
+    word_with_only_letters = string.downcase.gsub(/[^a-z]/, "")
+    word_array = word_with_only_letters.split("")
+    word_array.each do |letter|
+      letter_counts[letter] += 1
+    end
   end
   letter_counts
 end
+
 
 test_word = "Housecall Pro 12345!"
 p count_letters(test_word)
